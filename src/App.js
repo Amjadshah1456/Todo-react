5f817329236cb2ae3e824eb6efc265f0bd931ab4
Amjad Shah <shahg27059@gmail.com>
2022-12-04 18:01:07 +0500

    created todo-List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  
+const[inputArr, setInputArr] = useState ([]);
+ 
+const [inputData, setInputData] = useState( { name: "" , age: "" , designation: ""});
+  
+function handleChange(e) {
+setInputData({...inputData,[e.target.name]: e.target.value});
+}
+console.log(inputData);
+
+let { name, age, designation } = inputData;
+function handlleChange() {
+setInputArr([...inputArr, { name, age, designation }]);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Name:</p>
+      <input
+        type="text"
+        name="name"
+        onChange={handleChange}
+        value={inputData.name}
+      />
+      <p>Age:</p>
+
+      <input
+        type="number"
+        name="age"
+        onChange={handleChange}
+        value={inputData.age}
+      />
+      <p>Designation:</p>
+
+      <input
+        type="text"
+        name="designation"
+        onChange={handleChange}
+        value={inputData.designation}
+      /> <br/><br/>
+      <button onClick={handlleChange}>Add</button>
+
+      <br/><br/>
+      <table border={1} width="10%" cellPadding={10} >
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Designation</td>
+          </tr>
+          {inputArr.map((info, ind) => {
+            return (
+              <tr key={ind}>
+                <td>{info.name}</td>
+                <td>{info.age}</td>
+                <td>{info.designation}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
